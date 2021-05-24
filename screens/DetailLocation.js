270ae90de3a582d368a2292a17881624ef3cb0e8
ttnhat17it3 <38066091+ttnhat17it3@users.noreply.github.com>
2021-05-24 11:59:12 +0700

    Home Firebase

--- screens/DetailLocation.js
+++ screens/DetailLocation.js
@@ -3,8 +3,11 @@ import { FlatList, Text, View, Image, StyleSheet, SafeAreaView, ScrollView, Butt
 import Star from 'react-native-star-view';
 // import { Entypo, AntDesign } from '@expo/vector-icons';
 import MapView from 'react-native-maps';
+import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+import { auth } from "../firebase/firebase";
 
-export default function DetailLocation() {
+export default function DetailLocation({ navigation, route }) {
 
     const getInitialState = {
         latitude: 15.98,
@@ -14,6 +17,10 @@ export default function DetailLocation() {
     };
     const [location, onChangeLocation] = React.useState(getInitialState);
     const [paddingTop, onChangePaddingTop] = React.useState(0);
+    const [detailLocation, setDetailLocation] = React.useState({});
+    const [id, setId] = React.useState(route.params.id)
+    const [images, setImages] = React.useState([])
+
     const onRegionChange = (location) => {
         onChangeLocation(location);
     }
@@ -43,9 +50,60 @@ export default function DetailLocation() {
         }
     }
 
+    const prepareUploadImageToStorage = () =>{
+        let app;
+        var stCredentials ={
+            apiKey: "AIzaSyAcH9iGfbmP1Xzx8j5OB1wNyGTkHoCAvmk",
+            appId:"1:138826178666:web:62961ee1ec17c2899faa13",
+            authDomain: "qrtravel-vku.firebaseapp.com",
+            databaseURL: "https://qrtravel-vku-default-rtdb.firebaseio.com",
+            storageBucket: "qrtravel-vku.appspot.com",
+            messagingSenderId: "138826178666",
+            projectId: "qrtravel-vku",
+            measurementId: "G-9ZZVLC2KNJ"
+        }
+        if(firebase.apps.length === 0){
+            app = firebase.initializeApp(stCredentials);
+        }else{
+            app = firebase.app();
+        }
+        return app;
+    }
+    const loadData = async () => {
+        const user = await auth.currentUser;
+        const locationList = []
+        console.log("Get data!")
+        if (user != null) {
+            // console.log(user.email);
+            const app = prepareUploadImageToStorage();
+            const location = await firestore()
+                .collection('location')
+                .doc(id)
+                .get()
+                .then(snapshot => {
+                    console.log("Detail Location: ", snapshot.data())
+                    setDetailLocation({...snapshot.data()})
+                    const img = detailLocation.image
+                    setImages([...img])
+                    console.log("Image: ", images)
+                })
+                .catch(err => {
+                    console.log('Error getting documents', err);
+                });
+            // console.log("Test")    
+            // console.log(locationList.image)
+        }else{
+            console.log("Null");
+        }
+        
+    }
+
     React.useEffect(() => {
+        console.log("ID: ", route.params.id)
+        loadData()
+        // console.log("Image: ", detailLocation.image)
         onRegionChange(location);
-    });
+    }, []);
 
     return (
         <ScrollView style={[styles.container, {
@@ -89,10 +147,31 @@ export default function DetailLocation() {
             </View>
             <View>
                 <SafeAreaView>
+                    {/* <FlatList
+                        data={images}
+                        renderItem={(image) => (
+                            <Image
+                                style={{
+                                    height: 150,
+                                    width: 250
+                                }}
+                                source={{ uri: image }} 
+                            />
+                        )}
+                    /> */}
                     <ScrollView
                         style={styles.scrollView}
                         horizontal={true}>
-                        <Image
+                        {images.map((item) => (
+                            <Image
+                                style={{
+                                    height: 150,
+                                    width: 250
+                                }}
+                                source={{ uri: item }} 
+                            />
+                        ))}
+                        {/* <Image
                             style={{
                                 height: 150,
                                 width: 250
@@ -109,23 +188,23 @@ export default function DetailLocation() {
                                 height: 150,
                                 width: 200
                             }}
-                            source={{ uri: 'https://s3-alpha-sig.figma.com/img/6c09/8f9d/6c0e4ed05381f329c92ee4a541f81e37?Expires=1619395200&Signature=YYX2N~Gj~qEyVQni03A-W76wNHtOUUaLARBvfYKQhoFJpYgymhrMqyfrk3meAMubblcDORe3CD014g1V9wXrRtj90fynvY-1x3rzE0YwNlnntan89flypsPEsnmoNLL4uGtbGHssQ4Kvv-Zt~iudlSFTNpWwv0isVefCU2f89YIXGfftwJJPIakpydVXYB7Un7jvrCK7DE1G2rGDLGQWgMKcZ9vkHX0gX2Mv-v7swLng51QL5q9qge6-I551JAQh07ZI7OxPZicduFlvrP2iPzoc3ccNfsViSjgUBcG0YS-JUD2toN~SWQUk8bZaRJQbUGvp5HZMuHG9Rj3pNRG3HQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA', }} />
+                            source={{ uri: 'https://s3-alpha-sig.figma.com/img/6c09/8f9d/6c0e4ed05381f329c92ee4a541f81e37?Expires=1619395200&Signature=YYX2N~Gj~qEyVQni03A-W76wNHtOUUaLARBvfYKQhoFJpYgymhrMqyfrk3meAMubblcDORe3CD014g1V9wXrRtj90fynvY-1x3rzE0YwNlnntan89flypsPEsnmoNLL4uGtbGHssQ4Kvv-Zt~iudlSFTNpWwv0isVefCU2f89YIXGfftwJJPIakpydVXYB7Un7jvrCK7DE1G2rGDLGQWgMKcZ9vkHX0gX2Mv-v7swLng51QL5q9qge6-I551JAQh07ZI7OxPZicduFlvrP2iPzoc3ccNfsViSjgUBcG0YS-JUD2toN~SWQUk8bZaRJQbUGvp5HZMuHG9Rj3pNRG3HQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA', }} /> */}
                     </ScrollView>
                 </SafeAreaView>
                 <SafeAreaView style={styles.information}>
                     <View style={styles.headOption}>
-                        <Text style={styles.title} >Asia Park – Sunworld Đà Nẵng Wonders</Text>
+                        <Text style={styles.title} >{detailLocation.name}</Text>
                     </View>
                     <View style={styles.detailInformation}>
                         <View style={{ flex: 3, }} >
                             <View
                                 style={{ flexDirection: 'column' }}>
-                                <Text style={{ fontSize: 12 }}>Khu du lịch Bà Nà Hills, thôn An Sơn, Xã Hoà Ninh, Huyện Hoà Vang, TP. Đà Nẵng</Text>
+                                <Text style={{ fontSize: 12 }}>{detailLocation.address}</Text>
                             </View>
                             <View style={styles.starAndView}>
                                 <View style={{ flexDirection: 'row' }}>
-                                    <Star score={4,3} style={styles.starStyle} />
-                                    <Text style={{ marginTop: 3 }}> 4.3</Text>
+                                    <Star score={detailLocation.rating} style={styles.starStyle} />
+                                    <Text style={{ marginTop: 3 }}> {detailLocation.rating}</Text>
                                 </View>
                                 <View style={{ flexDirection: 'row' }}>
                                     {/* FIXME: Replace with another fonts */}
@@ -157,7 +236,7 @@ export default function DetailLocation() {
                 </SafeAreaView>
                 <SafeAreaView>
                     <Text style={styles.textContentView}>
-                       {'\t'}Công viên Châu Á, Asia Park - Sun World Danang Wonders mang đến cho bạn cơ hội trải nghiệm hàng chục hoạt động giải trí cảm giác mạnh lần đầu xuất hiện tại Việt Nam. Bắt đầu với Queen Cobra - đường tàu lượn siêu tốc với vận tốc tối đa 80km/h, đỉnh cao nhất 36m và mất 96 giây để thực hiện những cú lộn vòng giữa không trung đầy ngoạn mục. Flying Kirins mô phỏng con thuyền giữa những đợt sóng dữ dội của đại dương. Singapore Sling đưa người chơi lên độ cao hơn 30m, xoay tròn rồi rơi tự do với tốc độ 65 km/h đầy mạo hiểm.
+                       {'\t'}{detailLocation.detail}
                     </Text>
                 </SafeAreaView>
             </View>
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import {  Image, Text, View, StyleSheet, FlatList, SafeAreaView, TouchableOpacity, Dimensions, TextInput, ImageBackground, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { auth, db } from "../firebase/firebase";
+import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+import { createIconSetFromFontello } from "react-native-vector-icons";
 // StatusBar.setHidden(true);StatusBar,
 
 const windowWidth = Dimensions.get('screen').width;
@@ -39,17 +42,85 @@ const Item = ({ name, img, add, navigation, screen }) => (
     </TouchableOpacity>
 );
 
+const loadLocation = async()=>{
+    const locationList = await firestore().collection('location').get();
+    console.log(locationList.data)
+}
+
 export default function HomeScreen({ navigation }) {
 
     const [search, setSearch] = React.useState("")
+    const [location, setLocation] = React.useState([])
+    const [post, setPost] = React.useState([])
 
     const updateSearch = (search) => {
         setSearch(search)
     }
 
     const renderItem = ({ item }) => (
-        <Item name={item.name} img={item.img} add={item.add} navigation={navigation}/>
+        <Item name={item.name} img={item.thumbnail} add={item.address} navigation={navigation}/>
     );
+
+    const prepareUploadImageToStorage = () =>{
+        let app;
+        var stCredentials ={
+            apiKey: "AIzaSyAcH9iGfbmP1Xzx8j5OB1wNyGTkHoCAvmk",
+            appId:"1:138826178666:web:62961ee1ec17c2899faa13",
+            authDomain: "qrtravel-vku.firebaseapp.com",
+            databaseURL: "https://qrtravel-vku-default-rtdb.firebaseio.com",
+            storageBucket: "qrtravel-vku.appspot.com",
+            messagingSenderId: "138826178666",
+            projectId: "qrtravel-vku",
+            measurementId: "G-9ZZVLC2KNJ"
+        }
+        if(firebase.apps.length === 0){
+            app = firebase.initializeApp(stCredentials);
+        }else{
+            app = firebase.app();
+        }
+        return app;
+    }
+
+    const loadData = async () => {
+        // const user = await auth.currentUser;
+        const locationList = []
+        const postList = []
+        console.log("Get data!")
+        // if (user != null) {
+            // console.log(user.email);
+            const app = prepareUploadImageToStorage();
+            try {
+                var locationSnapshot = await firestore().collection("location").get();
+                var postSnapshot = await firestore().collection("posts").get();
+                console.log("Here");
+                locationSnapshot.forEach((doc) => {
+                    locationList.push({
+                        ...location,
+                        id: doc.id,
+                        ...doc.data()
+                    });
+                });
+                setLocation([...locationList]);
+                // console.log("Locations: ", location)
+
+                postSnapshot.forEach((doc) => {
+                    postList.push({
+                        id: doc.id,
+                        ...doc.data()
+                    });
+                });
+                setPost([...postList]);
+                // console.log("Posts: ", post)
+            } catch (error) {
+                console.log(error)
+            }
+        
+    }
+
+    React.useEffect(() => {
+        // getData()
+        loadData()
+    }, [])
 
     return (
         <SafeAreaView >
@@ -79,7 +150,7 @@ export default function HomeScreen({ navigation }) {
                     </View>
                     <View >
                         <FlatList 
-                            data={categories}
+                            data={post}
                             renderItem={({item}) => (
                                 <TouchableOpacity 
                                     style={{paddingRight: 10}} 
@@ -88,13 +159,13 @@ export default function HomeScreen({ navigation }) {
                                     })}
                                 >
                                     <ImageBackground 
-                                        source={{uri: item.img}}
+                                        source={{uri: item.image}}
                                         style={{width: 100, height: 130, }}
                                         imageStyle={{
                                             borderRadius: 10
                                         }}>
                                         <View style={{position: 'absolute', left: 0, right: 0, bottom: 15, justifyContent: 'center', alignItems: 'center'}}>
-                                            <Text style={{color: "white", fontWeight: "bold"}}>TP California</Text>
+                                            <Text style={{color: "white", fontWeight: "bold"}}>{item.title}</Text>
                                         </View>
                                     </ImageBackground>
                                     
@@ -108,7 +179,7 @@ export default function HomeScreen({ navigation }) {
             
                 <View style={{padding: 10}}>
                     <View style={styles.titleContainer}>
-                        <Text style={styles.title}>Các hoạt động nổi bật</Text>
+                        <Text style={styles.title}>Địa điểm mới</Text>
                         <Text style={styles.seeMore}>Xem thêm</Text>
                     </View>
                     <View >
@@ -159,8 +230,27 @@ export default function HomeScreen({ navigation }) {
                     </View>                                
                     <FlatList
                         style={styles.listView}
-                        data={categories}
-                        renderItem={renderItem}
+                        data={location}
+                        // renderItem={renderItem}
+                        renderItem={({item}) => (
+                            <TouchableOpacity 
+                                style={styles.container}
+                                onPress={() => {
+                                    console.log(item.id)
+                                    navigation.navigate('DetailLocation', {
+                                        id: item.id
+                                    })
+                                }}
+                            >
+                                <Image source={{uri: item.thumbnail}}
+                                    style={styles.Catimg}
+                                />
+                                <View style={styles.cont}>
+                                    <Text style={styles.nameqr}>{item.name}</Text>
+                                    <Text style={styles.nameadd}>{item.address}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        )}
                         keyExtractor={item => item.id.toString()}
                     />
                 </View>

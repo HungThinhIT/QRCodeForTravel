87c822aaab8ae8eb402e97b511f943147170a2f6
vvnha <38065969+vvnha@users.noreply.github.com>
2021-05-26 16:49:26 +0700

    update firebase favorite

--- screens/favorite.js
+++ screens/favorite.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
-import {  Image, Text, View, StyleSheet, FlatList, SafeAreaView, TouchableOpacity, Dimensions, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {  Image, Text, View, StyleSheet, FlatList, ScrollView, TouchableOpacity, Dimensions, TextInput } from 'react-native';
 import { firebase, firestore, auth} from "../firebase/firebase";
+import Star from 'react-native-star-view';
 // StatusBar.setHidden(true);StatusBar,
 
     const categories = [
@@ -15,33 +16,53 @@ import { firebase, firestore, auth} from "../firebase/firebase";
 const loadFavor = async()=>{
     const user = await auth().currentUser;
     if (user != null) {
-        console.log(user.email);
-        const app = prepareUploadImageToStorage();
+        var region = [];
         const favor = await firestore().collection('users').doc(user.email).get();
         const data = favor._data.favorite_locations;
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
+            const loca = await firestore().collection('location').doc(data[i]).get().then(data=>{
+                var specificRegion = {
+                    id: data[i],
+                    data: data._data
+                }
+                region.push(specificRegion);
+            })
         }
     }else{
         console.log("Null");
     }
-    
+    return region;
 }
 
-const Item = ({ name, img,add }) => (
-    <TouchableOpacity style={styles.container} onPress={() => { loadFavor() }}>
+const Item = ({ navigation, id, name, img,add,rate, numRate,  }) => (
+    <TouchableOpacity style={styles.container} onPress={() => { loadFavor(navigation, id) }}>
         <Image source={{uri: img}}
             style={styles.Catimg}
         />
         <View style={styles.cont}>
             <Text style={styles.nameqr}>{name}</Text>
             <Text style={styles.nameadd}>{add}</Text>
+            <View style={styles.starAndView}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Star score={rate} style={styles.starStyle} />
+                    <Text style={{ marginTop: 3 }}> {rate}</Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                                    {/* FIXME: Replace with another fonts */}
+                                    {/* <Entypo name="eye" size={24} color="black" /> */}
+                    <Text></Text>
+                    <Text style={{ marginTop: 3 }}> {numRate}</Text>
+                </View>
+            </View>
+
         </View>
     </TouchableOpacity>
 );
 
 export default function Favorite({ navigation }) {
+    const [favorities, setFavorities] = useState([]);
     React.useEffect(() => {
+        loadFavor().then(data=>{setFavorities(data);})
         const unsubscribe = navigation.addListener('focus', () => {
             auth().onAuthStateChanged(function(user) {
                 if (!user) {
@@ -53,17 +74,18 @@ export default function Favorite({ navigation }) {
       }, [navigation]);
 
     const renderItem = ({ item }) => (
-        <Item name={item.name} img={item.img} add={item.add}/>
+        <Item navigation={navigation} id={item.id} name={item.data.name} img={item.data.thumbnail} add={item.data.address} rate={item.data.rating} numRate={item.data.user_rating.length}/>
     );
     return (
-        <SafeAreaView >
+        <ScrollView >
+            <View style={styles.backgroundBorder} />
             <FlatList
                 style={styles.listView}
-                data={categories}
+                data={favorities}
                 renderItem={renderItem}
-                keyExtractor={item => item.id.toString()}
+                keyExtractor={item => item.id}
             />
-        </SafeAreaView>
+        </ScrollView>
     );
 }
 const styles = StyleSheet.create({
@@ -78,10 +100,29 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         
     },
+    
+    headerTitle: {
+        fontSize: 18,
+        color: 'black',
+    },
+    titleContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingBottom: 10
+    },
+    title: {
+        paddingTop:5,
+        paddingLeft:10,
+        fontSize: 18
+    },
+    seeMore: {
+        color: "#0A7FD9"
+    },
     Catimg:{
-        width:60,
-        height:60,
-        flex: 1,
+        width: 76,
+        height: 80,
     },
     nameqr:{
         marginLeft:10,
@@ -94,6 +135,42 @@ const styles = StyleSheet.create({
         marginLeft:10,
         flexGrow:2,
         flex: 2,
+    },
+    
+    starAndView: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        width: '90%',
+        paddingTop: 5
+    },
+    starStyle: {
+        width: 100,
+        height: 20,
+        marginBottom: 10,
+        marginLeft:10
+    },
+
+    searchContainer: {
+        backgroundColor:"#FFFFFF",
+        display: "flex",
+        flexDirection: "row",
+        // justifyContent: "center",
+        paddingLeft: 10,
+        alignItems: "center",
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#0A7FD9",
+        margin: 10
+    },
+    search: {
+        // margin:10,
+        // height: 50,
+        // borderWidth: 1,
+        // borderRadius: 20,
+        // paddingHorizontal: 12,
+        borderColor: '#0A7FD9',
+        borderRadius: 10,
+        fontSize:14
     },
     cont:{
         flexDirection: "column",
